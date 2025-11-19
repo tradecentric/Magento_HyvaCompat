8183d840384cdb26f53571e388774e84a4aabdda
dan bosler <dan.bosler@tradecentric.com>
2025-11-19 13:34:52 -0600

    CN-938_hyva_ext

---
+++ Helper/Module.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Punchout2Go\HyvaCompat\Helper;
+
+use TradeCentric\Version\Helper\ModuleHelper;
+
+/**
+ * Class Module
+ * @package Punchout2Go\HyvaCompat\Helper
+ */
+class Module extends ModuleHelper
+{
+
+}

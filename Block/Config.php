101f2e67fe997bcc9ab9a68224829a60b7d3207f
dan bosler <dan.bosler@tradecentric.com>
2025-12-08 14:16:59 -0600

    CN-942_js_logging

--- Block/Config.php
+++ Block/Config.php
@@ -9,17 +9,23 @@ class Config extends Template
 {
     const XML_PATH_REST_STORE_CODE = 'punchout2go_punchout/hyvacompat/rest_store_code';
     const XML_PATH_API_KEY_OVERRIDE = 'punchout2go_punchout/hyvacompat/api_key';
-    // Best-guess of original module path
     const XML_PATH_API_KEY_ORIGINAL = 'punchout2go_punchout/general/api_key';
 
     protected $scopeConfig;
+	
+	/**
+     * @var \Punchout2Go\Punchout\Helper\Transfer
+     */
+    protected $transferHelper;
 
     public function __construct(
         Template\Context $context,
         ScopeConfigInterface $scopeConfig,
+		\Punchout2Go\Punchout\Helper\Transfer $transferHelper,
         array $data = []
     ) {
         $this->scopeConfig = $scopeConfig;
+		$this->transferHelper = $transferHelper;
         parent::__construct($context, $data);
     }
 
@@ -40,4 +46,20 @@ class Config extends Template
     {
         return $this->getUrl('punchout/session/closePost');
     }
+	
+    /**
+     * @return int
+     */
+    public function getIsDebug()
+    {
+        return (int) $this->transferHelper->getIsDebug();
+    }
+
+    /**
+     * @return int
+     */
+    public function getIsJsLogging()
+    {
+        return (int) $this->transferHelper->getIsJsLogging();
+    }	
 }
